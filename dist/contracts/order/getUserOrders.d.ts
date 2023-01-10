f3483c6bfc583aa73c1b991cd5573d95f3ba0627
Dmitry Kanaev <dimmkan@mail.ru>
2023-01-10 10:37:29 +0300

    Get user orders - fix #4

--- dist/contracts/order/getUserOrders.d.ts
+++ dist/contracts/order/getUserOrders.d.ts
@@ -1,19 +1,18 @@
-export declare type OrdersList = {
-    id: number;
-    date_created: number | null;
-    date_updated: number | null;
-    user_id: number;
-    employee_id: number | null;
-    theme: string;
-    description: string;
-    status: string;
-};
 export declare namespace GetUserOrders {
     const topic = "order.getbyuser.query";
     class Request {
         user_id: number;
     }
     class Response {
-        data: OrdersList[];
+        data: {
+            id: number;
+            date_created: number | null;
+            date_updated: number | null;
+            user_id: number;
+            employee_id: number | null;
+            theme: string;
+            description: string;
+            status: string;
+        }[];
     }
 }
